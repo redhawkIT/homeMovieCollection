6f9cbc96373c946636a297e2ec87ca9242b2f133
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-08-31 15:20:10 -0500

    toggleview between AddMovie and MovieList

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
+import MovieList from './components/MovieList';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: {}
+      movies: {},
+      view: false
     };
+  }
+
+  _toggleView() {
+    this.setState({view: !this.state.view})
+    console.log(this.state.view)
   }
 
   render() {
     return (
       <div>
-        <NavBar/>
+        <NavBar toggleView={this._toggleView.bind(this)}/>
         <SearchBar/>
+        {this.state.view ? <AddMovie/> : <MovieList/>}
       </div>
     );
   }
