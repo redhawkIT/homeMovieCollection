a379b36685bf0071ab4c028becd4e82db1dcf392
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-08-31 16:26:44 -0500

    add forms

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -10,7 +10,43 @@ export default class AddMovie extends Component {
 
   render() {
     return (
-      <h1>Add Movie</h1>
+      <form className="form-horizontal">
+        <div className="form-group">
+          <label className="control-label col-sm-2">Title:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="email" placeholder="Enter Title"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-2">Year:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="email" placeholder="Enter Year"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-2">Rating:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="pwd" placeholder="Enter Rating"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-2">Genre:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="pwd" placeholder="Enter genre"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-2">Actors:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="pwd" placeholder="Enter actors"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <button type="submit" className="btn btn-default">Submit</button>
+          </div>
+        </div>
+      </form>
     );
   }
 }
