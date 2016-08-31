33169b58fef6935b619ebaa0ff175ed3dc80efdd
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-08-31 15:20:36 -0500

    remove logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,6 @@ import React, { Component } from 'react';
 
 export default class NavBar extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: ''
-    };
-  }
-
-  _handleSubmit(event) {
-    event.preventDefault();
-    this._addMovie(this.state.term);
-  }
-
-  _onInputChange(term) {
-    this.setState({term});
-  }
-
-  _addMovie(title) {
-
-  }
-
   render() {
     return (
       <nav className="navbar navbar-inverse">
@@ -32,15 +12,8 @@ export default class NavBar extends Component {
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <form className="navbar-form navbar-left" onSubmit={this._handleSubmit.bind(this)}>
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  placeholder="Add Movie"
-                  onChange={event => this._onInputChange(event.target.value)}
-                />
-              </div>
-              <button type="submit" className="btn btn-default">Submit</button>
+            <form className="navbar-form navbar-left">
+              <button className="btn btn-default" onClick={this.props.toggleView.bind(this)}>Add Movie</button>
             </form>
           </div> {/* <!-- /.navbar-collapse -->*/}
         </div> {/* <!-- /.container-fluid --> */}
