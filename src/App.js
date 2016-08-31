2924df7359670f779f4739a12649d9462efe05a5
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-08-31 14:50:09 -0500

    add navbar and searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: {}
+    };
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <NavBar/>
+        <SearchBar/>
+      </div>
     );
   }
 }
