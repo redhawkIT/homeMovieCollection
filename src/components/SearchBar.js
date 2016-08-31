23839808922e09b526d415b45b80b5ac8ceaa906
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-08-31 16:26:54 -0500

    remove col and row

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,21 +10,17 @@ export default class SearchBar extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col-lg-6">
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="Search Movies"/>
-            <div className="input-group-btn">
-              <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options <span className="caret"></span></button>
-              <ul className="dropdown-menu dropdown-menu-right">
-                <li><a href="#">Year</a></li>
-                <li><a href="#">Rating</a></li>
-                <li><a href="#">Actors</a></li>
-                <li><a href="#">Genre</a></li>
-              </ul>
-            </div>{/* <!-- /btn-group --> */}
-          </div>{/* <!-- /input-group --> */}
-        </div>{/* <!-- /.col-lg-6 --> */}
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Filter by Title"/>
+        <div className="input-group-btn">
+          <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Options <span className="caret"></span></button>
+          <ul className="dropdown-menu dropdown-menu-right">
+            <li><a href="#">Year</a></li>
+            <li><a href="#">Rating</a></li>
+            <li><a href="#">Actors</a></li>
+            <li><a href="#">Genre</a></li>
+          </ul>
+        </div>
       </div>
     );
   }
