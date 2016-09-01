b32894b9db97a5b1eab47a215334240c568de5f7
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-01 14:49:29 -0500

    update list item

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,9 @@ export default class Movie extends Component {
     return (
       <a href="#" className="list-group-item Movie">
         <div id="Movie-Title">{`${title} (${year})`}</div>
-        <div id="Movie-Rating">{rating}</div>
+        <div id="Movie-Rating">{`Rating: ${rating}`}</div>
+        <div id="Movie-Actors">{`Staring: ${actors.join(', ')}`}</div>
+        <div id="Movie-genre">{`Genre: ${genre}`}</div>
       </a>
     );
   }
